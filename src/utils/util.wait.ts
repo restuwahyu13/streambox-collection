4072c4727e8d0c93baa7d68421027e97af9d9515
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-28 01:52:18 +0700

    fix typo script command

--- src/utils/util.wait.ts
+++ src/utils/util.wait.ts
@@ -2,4 +2,4 @@ import wait, { ClearablePromise } from 'delay'
 import { isType } from './util.is'
 
 export { ClearablePromise }
-export const waitFor = (delay: number): ClearablePromise<void> => isType(delay) === 'number' ? wait(delay) : wait(0)
+export const waitFor = (delay: number): ClearablePromise<void> => (isType(delay) === 'number' ? wait(delay) : wait(0))
